8267076d29b94d5a344682102db3808adf9d4f47
Emils <emilsberzs@hotmail.com>
2023-02-22 08:15:13 +0000

    Game grid working, only one mark per square, switches player after each move, need to implement game logic and compartamentalise code

--- script.js
+++ script.js
@@ -1,18 +1,3 @@
-//FACTORY
-const Player = (name, symbol) => {
-
-    const announce = () => console.log(`${name} plays with ${symbol}`);
-    return { name, symbol, announce }
-}
-
-const playerOne = Player('emils', 'X')
-const playerTwo = Player('rausis', 'O')
-
-playerOne.announce()
-playerTwo.announce()
-
-let active_player = playerOne;
-
 const square_1 = document.getElementById('square_1')
 const square_2 = document.getElementById('square_2')
 const square_3 = document.getElementById('square_3')
@@ -23,50 +8,56 @@ const square_7 = document.getElementById('square_7')
 const square_8 = document.getElementById('square_8')
 const square_9 = document.getElementById('square_9')
 
+//FACTORY
+const Player = (name, symbol) => {
 
-square_1.addEventListener('click', (event) => {
-    if (active_player == playerOne) {
-        square_1.innerHTML = active_player.symbol;
-        active_player = playerTwo;
-    }
-    else if (active_player == playerTwo) {
-        square_1.innerHTML = active_player.symbol;
-        active_player = playerOne;
-    }
-});
-square_2.addEventListener('click', (event) => square_2.innerHTML = 'O')
-square_3.addEventListener('click', (event) => square_3.innerHTML = 'X')
-square_4.addEventListener('click', (event) => square_4.innerHTML = 'O')
-square_5.addEventListener('click', (event) => square_5.innerHTML = 'X')
-square_6.addEventListener('click', (event) => square_6.innerHTML = 'O')
-square_7.addEventListener('click', (event) => square_7.innerHTML = 'X')
-square_8.addEventListener('click', (event) => square_8.innerHTML = 'O')
-square_9.addEventListener('click', (event) => square_9.innerHTML = 'X')
+    const announce = () => console.log(`${name} plays with ${symbol}`);
+    return { name, symbol, announce }
+}
+
+//Initialize two players
+const playerOne = Player('emils', 'X')
+const playerTwo = Player('rausis', 'O')
+
+//Set initial active player
+let active_player = playerOne;
+
+
+//Function to mark a square
+function markSquare(square) {
+    square.addEventListener('click', (event) => {
+        if (square.innerHTML == '') {
+
+            if (active_player == playerOne) {
+                square.innerHTML = active_player.symbol;
+                active_player = playerTwo;
+            }
+            else if (active_player == playerTwo) {
+                square.innerHTML = active_player.symbol;
+                active_player = playerOne;
+            }
+        }
+    });
+}
+
+//Asign markSquare to all squares
+markSquare(square_1)
+markSquare(square_2)
+markSquare(square_3)
+markSquare(square_4)
+markSquare(square_5)
+markSquare(square_6)
+markSquare(square_7)
+markSquare(square_8)
+markSquare(square_9)
 
 
 
 //MODULE
 const gameBoard = (() => {
     const squares = {
-        1: { name: 'square_1', used: false, owner: undefined },
-        2: square_2,
-        3: square_3,
-        4: square_4,
-        5: square_5,
-        6: square_6,
-        7: square_7,
-        8: square_8,
-        9: square_9,
+
     }
-    const populate = () => {
-        for (value in squares) {
-            squares[value]
-            console.log(squares[value], value)
-        }
-    }
-    const describe = () => console.log(squares);
-    return { describe, populate };
 })();
 
-//gameBoard.describe()
-gameBoard.populate()
+
